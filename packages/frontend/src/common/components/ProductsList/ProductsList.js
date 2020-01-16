c4ce88ba5243360be82cbfef86ee4b807c1a269f
Vildan <gunv@xxxlgroup.com>
2020-01-16 16:56:53 +0100

    layout sort buttons

--- packages/frontend/src/common/components/ProductsList/ProductsList.js
+++ packages/frontend/src/common/components/ProductsList/ProductsList.js
@@ -38,6 +38,30 @@ class ProductsList extends React.Component {
     this.setState({ searchResult: event.target.value });
   };
 
+  ascendingName = () => {
+    const products = this.state.items;
+    const sortedList = products.sort((a, b) => a.name.localeCompare(b.name));
+    this.setState({ items: sortedList });
+  };
+
+  descendingName = () => {
+    const products = this.state.items;
+    const sortedList = products.sort((a, b) => b.name.localeCompare(a.name));
+    this.setState({ items: sortedList });
+  };
+
+  descendingPrice = () => {
+    const products = this.state.items;
+    const sortedList = products.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    this.setState({ items: sortedList });
+  };
+
+  ascendingPrice = () => {
+    const products = this.state.items;
+    const sortedList = products.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    this.setState({ items: sortedList });
+  };
+
   render() {
     const { error, isLoaded, items, searchResult } = this.state;
     if (error) {
@@ -54,11 +78,28 @@ class ProductsList extends React.Component {
       <>
         <Input
           type="text"
-          value={this.state.searchResult}
           onChange={this.updateSearch}
           label="Search"
           suffix={<Icon glyph={search} />}
         />
+        <div className={styles.btnBox}>
+          <div className={styles.btnSortPrice}>
+            <button type="button" className={styles.one} onClick={this.ascendingPrice}>
+              Ascending Price
+            </button>
+            <button type="button" className={styles.two} onClick={this.descendingPrice}>
+              Descending Price
+            </button>
+          </div>
+          <div className={styles.btnSortName}>
+            <button type="button" className={styles.three} onClick={this.ascendingName}>
+              A - Z
+            </button>
+            <button type="button" className={styles.four} onClick={this.descendingName}>
+              Z - A
+            </button>
+          </div>
+        </div>
 
         <div className={styles.containerList}>
           {filteredData.map(item => (
